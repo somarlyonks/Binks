d16082b861d1a5dc2c7c38445d499e51c2061dbe
sy <somarl@live.com>
2018-10-23 15:47:02 +0800

    feat(proxy): add proxy util

--- app/utils.py
+++ app/utils.py
@@ -3,6 +3,9 @@ import time
 import contextlib
 import sys
 import inspect
+import os
+import types
+from functools import wraps
 
 import requests as q
 
@@ -40,6 +43,70 @@ def stopwatch(message):
         print('[DROPBOX]: Total elapsed time for %s: %.3f' % (message, time.time() - t))
 
 
-def https_proxy(f):
-    """TODO"""
-    pass
+ENV_NAME = 'https_proxy'
+
+
+class Proxy(object):
+    """
+    Usage
+    ```python
+    with Proxy('https://127.0.0.1:8123'):
+        pass
+    ```
+    or
+    ```python
+    @Proxy('https://127.0.0.1:8123')
+    def func(x):
+        pass
+    ```
+    or
+    ```python
+    class Cls(object):
+        https_proxy = 'https://127.0.0.1:8123'
+
+        @Proxy
+        def method(self):
+            pass
+    ```
+    TODO
+      as cls decorator (has to rewrite the __new__)
+    """
+    def __init__(self, target):
+        self.location = getattr(target, ENV_NAME, target)
+        if type(self.location) is not str:
+            wraps(target)(self)
+            # self.__call__(target)
+
+    def __enter__(self):
+        self.former = os.getenv(ENV_NAME)
+        os.environ[ENV_NAME] = self.location
+        print('[BINKS]: Under proxy - ', self.location)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('[BINKS]: Exit proxy.')
+        del os.environ[ENV_NAME]
+        if self.former:
+            os.environ[ENV_NAME] = self.former
+
+    def __call__(self, *args, **kwargs):
+        target = args[0]
+
+        @wraps(target)
+        def wrapper(*args, **kwargs):
+            with self:
+                return target(*args, **kwargs)
+
+        if type(self.location) is not str:
+            self.location = getattr(target, ENV_NAME, target)
+            if type(self.location) is not str:
+                raise AttributeError(f'[PROXY]: target or target.{ENV_NAME} is supposed to be str.')
+
+        if getattr(self, '__wrapped__', None):
+            with self:
+                return self.__wrapped__(*args, **kwargs)  # pylint: disable=E1101
+        else:
+            return wrapper
+
+    def __get__(self, inst, cls):
+        return self if inst is None else types.MethodType(self, inst)
